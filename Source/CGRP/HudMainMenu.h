4fa41a28c6332e99d7d13e6ea9c5cf7c4af57e30
CallMeWho <lewxiuzhen@gmail.com>
2023-08-24 16:04:56 +0800

    Added: C++ Main Menu HUD
    -- added c++ HudMainMenu both .h and .cpp files

---
+++ Source/CGRP/HudMainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "hudMainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CGRP_API AhudMainMenu : public AHUD
+{
+	GENERATED_BODY()
+	
+};

a2641b9c51084e7763595d96797a6839835c9859
vw79 <wshong8282@gmail.com>
2023-08-29 01:36:52 +0800

    added music manager and audio source

--- Source/CGRP/Audio/MusicManager.h
+++ Source/CGRP/Audio/MusicManager.h
@@ -14,6 +14,25 @@ class CGRP_API AMusicManager : public AActor
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UAudioComponent* AudioComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Music")
+	USoundBase* MainMenuMusic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Music")
+	USoundBase* GameBGM;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Music")
+	USoundBase* WinMusic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Effects")
+	USoundBase* MoveChessSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Effects")
+	USoundBase* StartButtonSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Effects")
+	USoundBase* EndButtonSound;
+
 	// Sets default values for this actor's properties
 	AMusicManager();
 
@@ -21,8 +40,25 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintCallable, Category = "Music Control")
+	void PlayMainMenuMusic();
+
+	UFUNCTION(BlueprintCallable, Category = "Music Control")
+	void PlayGameBGM();
+
+	UFUNCTION(BlueprintCallable, Category = "Music Control")
+	void PlayWinMusic();
+
+	UFUNCTION(BlueprintCallable, Category = "Music Control")
+	void PlayMoveChessSound();
+
+	UFUNCTION(BlueprintCallable, Category = "Music Control")
+	void PlayStartButtonSound();
+
+	UFUNCTION(BlueprintCallable, Category = "Music Control")
+	void PlayEndButtonSound();
 };
